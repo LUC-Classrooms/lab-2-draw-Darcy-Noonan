a37cf960fa61c7c4e5390f13e25a1f3ecfbd7a38
Darcy-Noonan <156862020+Darcy-Noonan@users.noreply.github.com>
2024-01-23 03:46:25 +0000

    Square- Lizard? Where are triangles?

--- script.js
+++ script.js
@@ -7,5 +7,8 @@ function draw(){
   // add your drawing code here ...
   fill (200, 200, 20)
   ellipse (100, 50, 50)
-  rect(0,350,600)
+  rect(0,250,600)
+  rect (200,275,100)
+  Triangle (300, 275, 100, 300)?
+  Triangle (45, 340, 10)?
 }
